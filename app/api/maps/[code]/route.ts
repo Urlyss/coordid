ef768815889179ea498d8b1795b474679f9a58dd
Urlyss Kamto <34534180+Urlyss@users.noreply.github.com>
2025-07-21 16:15:48 +0100

    add CORS check
    Deleted the /api/countries endpoint, the useCountries hook, and the mongoDbInstance utility, likely as part of a refactor or deprecation of country-related features. Added CORS origin checking to the /api/maps/[code] endpoint to restrict access to allowed origins.

--- app/api/maps/[code]/route.ts
+++ app/api/maps/[code]/route.ts
@@ -1,11 +1,36 @@
 import { NextResponse } from 'next/server';
 import clientPromise from '@/lib/db';
 
+
 export async function GET(
     request: Request,
     { params }: { params: { code: string } }
 ) {
+        // List of allowed origins
+    const allowedOrigins = [process.env.PRODUCTION_URL || "http://localhost:3000"];
+    console.log(allowedOrigins)
+
+    // Function to check if the origin is allowed
+    function isAllowedOrigin(origin: string | null): boolean {
+    if (!origin) return false;
+    return allowedOrigins.some(allowedOrigin => origin.startsWith(allowedOrigin));
+    }
+
     try {
+        if (!isAllowedOrigin(request.headers.get('origin'))) {
+    return new NextResponse(
+      JSON.stringify({
+        success: false,
+        message: 'Unauthorized origin'
+      }),
+      {
+        status: 403,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+  }
         const client = await clientPromise;
         const db = client.db('coord');
 
